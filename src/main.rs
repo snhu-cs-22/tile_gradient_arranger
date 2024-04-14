2d0407ad46d4ddb17d3d25e09d2ebd7c89a2f3ef
snhu-cs-22 <110997198+snhu-cs-22@users.noreply.github.com>
2024-04-14 12:06:03 -0400

    Add printing of program progress.

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,17 @@ use tile_gradient_arranger::image::{read_images, write_image};
 
 fn main() {
     let args = Cli::parse();
+    eprintln!("Reading images...");
     let image_colors = read_images(
         &args.input,
         args.k_means,
         (args.tile_width, args.tile_height),
     );
     let image_count = image_colors.len();
+    eprintln!("Building graph...");
     let graph = build_graph(image_colors);
+    eprintln!("Arranging images...");
     let grid = arrange_images(&graph, image_count);
+    eprintln!("Writing image...");
     write_image(&grid, &args.output, (args.tile_width, args.tile_height));
 }
