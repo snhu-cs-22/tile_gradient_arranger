6b9ec8c7fa54ff0015adb30007432645f2965346
snhu-cs-22 <110997198+snhu-cs-22@users.noreply.github.com>
2024-05-05 13:20:06 -0400

    Fix over-reservation of edges when building graph
    The previous edge count was n^2 (where n is the number of nodes in the
    graph), which is much more than the number of edges in a complete graph
    (n(n - 1) / 2).

--- src/arrangement.rs
+++ src/arrangement.rs
@@ -32,7 +32,9 @@ pub fn arrange_images(graph: &ImageGraph, image_count: usize) -> OptionalGrid<&I
 }
 
 pub fn build_graph(image_colors: Vec<ImageColor>) -> ImageGraph {
-    let mut graph = ImageGraph::with_capacity(image_colors.len(), image_colors.len().pow(2));
+    let node_count = image_colors.len();
+    let edge_count = (node_count * (node_count - 1)) / 2;
+    let mut graph = ImageGraph::with_capacity(node_count, edge_count);
 
     // Add nodes
     for image_color in image_colors {
