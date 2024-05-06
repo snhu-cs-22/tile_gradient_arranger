e6fed3f27914b3ff31167687dfc044bf0c62cde4
snhu-cs-22 <110997198+snhu-cs-22@users.noreply.github.com>
2024-05-06 11:37:14 -0400

    Add laying out tiles in spiral pattern

--- src/arrangement.rs
+++ src/arrangement.rs
@@ -18,17 +18,83 @@ pub fn arrange_images(graph: &ImageGraph, image_count: usize) -> OptionalGrid<&I
         .node_indices()
         .max_by_key(|i| graph.neighbors(*i).count())
         .unwrap();
-    let mut build_order = Dfs::new(&graph, most_popular)
+    let build_order = Dfs::new(&graph, most_popular)
         .iter(&graph)
         .map(|i| &graph[i].image);
 
     // TODO: Arrange the images on the image grid
     let square_size = (image_count as f32).sqrt().ceil() as usize;
     let mut grid = Grid::init(square_size, square_size, None);
-    for cell in grid.iter_mut() {
-        *cell = build_order.next();
+    let coords = SpiralGridCoords::new((square_size / 2, square_size / 2));
+    for (image, coords) in build_order.zip(coords) {
+        grid[coords] = Some(image);
     }
     grid
+}
+
+#[derive(Clone, Debug, PartialEq)]
+enum Direction {
+    Down,
+    Right,
+    Up,
+    Left,
+}
+
+struct SpiralGridCoords {
+    current_coords: (usize, usize),
+    current_direction: Direction,
+    until_side_length: usize,
+    current_side_length: usize,
+}
+
+impl SpiralGridCoords {
+    pub fn new(start_coords: (usize, usize)) -> Self {
+        Self {
+            current_coords: start_coords,
+            current_direction: Direction::Down,
+            until_side_length: 0,
+            current_side_length: 0,
+        }
+    }
+
+    fn get_next(&mut self) -> (usize, usize) {
+        if self.current_side_length == 0 {
+            self.current_side_length += 1;
+            return self.current_coords;
+        }
+
+        self.current_coords = match self.current_direction {
+            Direction::Down => (self.current_coords.0, self.current_coords.1 + 1),
+            Direction::Right => (self.current_coords.0 + 1, self.current_coords.1),
+            Direction::Up => (self.current_coords.0, self.current_coords.1 - 1),
+            Direction::Left => (self.current_coords.0 - 1, self.current_coords.1),
+        };
+
+        self.until_side_length += 1;
+        if self.until_side_length == self.current_side_length {
+            self.until_side_length = 0;
+            if self.current_direction == Direction::Left
+                || self.current_direction == Direction::Right {
+                self.current_side_length += 1;
+            }
+            self.current_direction = match self.current_direction {
+                Direction::Down => Direction::Right,
+                Direction::Right => Direction::Up,
+                Direction::Up => Direction::Left,
+                Direction::Left => Direction::Down,
+            };
+        }
+
+        self.current_coords
+    }
+}
+
+impl Iterator for SpiralGridCoords {
+    type Item = (usize, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(self.get_next())
+    }
 }
 
 pub fn build_graph(image_colors: Vec<ImageColor>) -> ImageGraph {
